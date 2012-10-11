53be777860180eaf1aac15d05c3dbc1fcc705dc8
Curtis Ekstrom <ce@canvus.io>
2012-10-11 11:05:20 +0200

    add basic validations and tests to make sure message has bare minimum defined

--- app/models/communique/message.rb
+++ app/models/communique/message.rb
@@ -2,5 +2,8 @@ module Communique
   class Message < ActiveRecord::Base
     attr_accessible :content, :deleted, :draft, :sender_id, :subject
     attr_accessor :recipients
+
+    belongs_to :sender, class_name: "::User"
+    validates_presence_of :sender_id, :recipients, :subject, :content
   end
 end
--- spec/models/communique/message_spec.rb
+++ spec/models/communique/message_spec.rb
@@ -4,4 +4,16 @@ describe Communique::Message do
   it "has a valid factory" do
     build(:message).should be_valid
   end
+  it "is invalid without a sender" do
+    build(:message, sender_id: nil).should_not be_valid
+  end
+  it "is invalid without at least one recipient" do
+    build(:message, recipients: nil).should_not be_valid
+  end
+  it "is invalid without a subject" do
+    build(:message, subject: nil).should_not be_valid
+  end
+  it "is invalid without message content" do
+    build(:message, content: nil).should_not be_valid
+  end
 end
