f431df3a42a3a8ef8b6c1489ea8ad116c8463753
Curtis Ekstrom <ce@canvus.io>
2012-10-11 09:36:39 +0200

    added rspec

--- lib/communique/engine.rb
+++ lib/communique/engine.rb
@@ -1,5 +1,10 @@
 module Communique
-  class Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine    
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
+
     isolate_namespace Communique
   end
 end
