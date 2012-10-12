c4917c23cec6c4d065b19e042e86253bd1907dfc
Curtis Ekstrom <ce@canvus.io>
2012-10-12 21:24:06 +0200

    Minor changes: Add locales to mimic gmail date/time functionality reformat controller code

--- app/controllers/communique/messages_controller.rb
+++ app/controllers/communique/messages_controller.rb
@@ -5,52 +5,57 @@ module Communique
     before_filter :authenticate_user!
 
     def new
-        @message = Communique::Message.new
+      @message = Communique::Message.new
     end
   
     def edit
-        @message = Communique::Message.find(params[:id])
+      @message = Communique::Message.find(params[:id])
+      # render another page if trying to edit a message that was already sent
     end
   
     def update
-        @message = Communique::Message.find(params[:id])
+      @message = Communique::Message.find(params[:id])
+      # render another page if trying to edit a message that was already sent
     end
   
     def inbox
-        @messages = received_by_user
-        render :template => 'communique/messages/inbox'
+      @messages = received_by_user
+      render_inbox
     end
   
     def outbox
-        @messages = sent_by_user
-        render :template => 'communique/messages/inbox'
+      @messages = sent_by_user
+      render_inbox
     end
   
     def drafts
-        @messages = sent_by_user.draft
-        render :template => 'communique/messages/inbox'
+      @messages = sent_by_user.draft
+      render_inbox
     end
   
     def trash
-        @messages = sent_by_user.deleted | received_by_user.deleted
-        render :template => 'communique/messages/inbox'
+      @messages = sent_by_user.deleted | received_by_user.deleted
+      render_inbox
     end
   
     def read
-        @messages = received_by_user.read
+      @messages = received_by_user.read
     end
   
     def unread
-        @messages = received_by_user.unread
+      @messages = received_by_user.unread
     end
 
     private
 
     def sent_by_user
-        Communique::Message.sent_by(current_user)
+      Communique::Message.sent_by(current_user)
     end
     def received_by_user
-        Communique::Message.received_by(current_user)
+      Communique::Message.received_by(current_user)
+    end
+    def render_inbox
+      render :template => 'communique/messages/inbox'
     end
   end
 end
--- app/helpers/communique/messages_helper.rb
+++ app/helpers/communique/messages_helper.rb
@@ -1,4 +1,19 @@
 module Communique
   module MessagesHelper
+    def time_if_today(input_date)
+      dt = input_date.to_datetime
+      if input_date.day == DateTime.now.day
+        format_time(input_date)
+      else
+        format_date(input_date)
+      end
+    end
+    def format_time(input_date)
+      I18n.localize(input_date, format: :shortened_time)
+    end
+    def format_date(input_date)
+      I18n.localize(input_date, format: :shortened_day)
+    end
+
   end
 end
