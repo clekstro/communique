2f4582afab594c66bb4b66124743ba7a01b3ac76
Curtis Ekstrom <ce@canvus.io>
2013-01-04 07:08:34 +0100

    replace erb with slim

--- app/controllers/communique/messages/inbox_controller.rb
+++ app/controllers/communique/messages/inbox_controller.rb
@@ -8,13 +8,14 @@ module Communique
       end
 
       def show
-        prevent_recipient_forgery
-        @message.mark_as_read
+        @message = find_recipient_message
+        @message.mark_as_read if @message
+        redirect_to :messages_inbox_index
       end
 
       def reply
         original_message = Communique::SentMessage.find_by_id(params[:id])
-        if original_message && !prevent_reply_forgery(original_message)
+        if original_message && may_reply_to(original_message)
           @message = Communique::ReplyMessage.new(original_message)
           render template: 'communique/messages/drafts/new'
         else
@@ -23,24 +24,20 @@ module Communique
       end
 
       def trash
-        prevent_recipient_forgery
-        @message.mark_as_trashed
+        @message = find_recipient_message
+        @message.mark_as_trashed if @message
         redirect_to :messages_inbox_index
       end
 
       private
 
       # prevent anyone but recipient from viewing, replying to, trashing or deleting message
-      def prevent_recipient_forgery
-        @message = Communique::IncomingMessage.
-          find_by_id_and_recipient_id(params[:id], current_user_id)
-        redirect_to :messages_inbox_index and return unless @message
+      def find_recipient_message
+        Communique::IncomingMessage.find_by_id_and_recipient_id(params[:id], current_user_id)
       end
 
-      def prevent_reply_forgery(sent_message)
-        @message = Communique::IncomingMessage.find_by_recipient_id_and_message_id(
-          current_user_id, sent_message.id)
-        @message.nil?
+      def may_reply_to(sent_message)
+        !Communique::IncomingMessage.find_by_recipient_id_and_message_id(current_user_id, sent_message.id).nil?
       end
 
     end
